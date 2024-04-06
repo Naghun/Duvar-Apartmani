63698a95d9f9dd7e99002b215ed977c375bb1e26
Abdullah Sinanović <budad-97@hotmail.com>
2024-04-06 16:26:15 +0200

    Responsivenes done for apartment page

--- page-templates/page-apartman.php
+++ page-templates/page-apartman.php
@@ -43,27 +43,28 @@ $container = get_theme_mod( 'understrap_container_type' );
             if ($apartman_posts->have_posts()) :
                 while ($apartman_posts->have_posts()) : $apartman_posts->the_post();
             ?>
-                    <div class="apartman col-12 d-flex justify-content-start align-items-center">
-                        <div class="apartman-image col-4 d-flex justify-content-start align-items-start">
-                            <img src="<?php echo get_theme_file_uri('/src/images/placeholder-1.jpg'); ?>" alt="apartman placeholder slika" class="col-12">
+                    <div class="apartman col-12 d-flex justify-content-start align-items-start p-2">
+                        <div class="apartman-image col-5 d-flex justify-content-start align-items-start">
+                            <img src="<?php echo get_theme_file_uri('/src/images/kuca-slika-2.jpg'); ?>" alt="apartman placeholder slika" class="col-12">
                         </div>
-                        <div class="apartman-data col-8 d-flex flex-column justify-content-start align-items-start">
-                            <div class="apartman-data-header col-12 d-flex justify-content-center align-items-center">
-                                <h2 class=""><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <div class="apartman-data col-7 d-flex flex-column justify-content-between align-items-between">
+                            <div class="apartman-data-header col-12 d-flex justify-content-center align-items-center py-3   ">
+                                <h2><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?> - Galerija</a></h2>
                                 <div class="rating d-flex justify-content-center align-self-center fs-5"><span>9.0</span> <i class="fas fa-thumbs-up align-self-center"></i></div>
                             </div>
-                            <hr class="col-12">
+                            <hr class="col-12 p-0 m-0">
                             <div class="apartman-data-body d-flex col-12">
-                                <div class="body-left col-10 d-flex flex-column justify-content-start align-items-start g-2">
+                                <div class="body-left col-10 d-flex flex-column justify-content-start align-items-start g-2 py-2">
                                     <p class="text-center">Kvadratura: <?php echo get_field('kvadratura'); ?></p>
                                     <p class="text-center">Dodatna Oprema: <?php echo get_field('dodatna_oprema'); ?></p>
                                 </div>
                                 <div class="body-right col-2 d-flex flex-column align-items-end justify-content-end">
-                                    <p class="text-center align-self-bottom">Cijena: <?php echo get_field('cijena'); ?>KM</p>
+                                    <p class="text-center align-self-bottom px-2">Cijena: <?php echo get_field('cijena'); ?>KM</p>
                                 </div>
-
+                            
                             </div>
-                            <div class="apartman-data-footer col-12 d-flex justify-content-end align-items-center">
+                            <hr class="col-12 p-0 m-0 my-3">
+                            <div class="apartman-data-footer col-12 d-flex justify-content-end align-items-center  py-3 px-2">
                                 <button class="btn btn-dark p-2 fs-4">Pogledaj Dostupnost</button>
                             </div>
 
